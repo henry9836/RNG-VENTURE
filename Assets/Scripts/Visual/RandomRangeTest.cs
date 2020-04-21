9ec50c111345495c2fdade2b2170e11cc9f49c9e
Vaughan Webb <vaughanw2012@gmail.com>
2020-04-22 10:58:48 +1200

    mouse move and a selection box for modes added

--- Assets/Scripts/Visual/RandomRangeTest.cs
+++ Assets/Scripts/Visual/RandomRangeTest.cs
@@ -12,10 +12,41 @@ public class RandomRangeTest : MonoBehaviour
 
     public List<Vector3> spawns = new List<Vector3>();
 
+    private float v1 = 0.0f;
+    private float v2 = 0.0f;
+    private float v3 = 0.0f;
+
+    public enum method
+    {
+        LCG,
+        URANIUM,
+        MOUSE,
+    }
+
+    public method chosen;
+
 
 
     // Start is called before the first frame update
     void Start()
+    {
+        if (chosen == method.LCG)
+        {
+            LCG();
+        }
+        else if (chosen == method.URANIUM)
+        {
+            uranium();
+        }
+        else if (chosen == method.MOUSE)
+        {
+            mouse();
+        }
+
+    }
+
+
+    private void LCG()
     {
         //Debug.Log($"B: {Random.state}");
         //Random.InitState(1234);
@@ -56,9 +87,98 @@ public class RandomRangeTest : MonoBehaviour
 
             spawns.Add(new Vector3((float)v1, (float)v2, (float)v3));
         }
-        Debug.Log($"The biggest boi is: {bigboi}");
+
+
         StartCoroutine(SpawnObjects());
+
     }
+
+    private void uranium()
+    {
+        //TODO add code
+    }
+
+    private void mouse()
+    {
+        StartCoroutine(mousewig());
+        StartCoroutine(SpawnObjects());
+
+    }
+
+    void Number(float tmp)
+    {
+        if (v1 == 0.0f)
+        {
+            v1 = tmp;
+        }
+        else if (v2 == 0.0f)
+        {
+            v2 = tmp;
+        }
+        else if (v3 == 0.0f)
+        {
+            v3 = tmp;
+        }
+    }
+ 
+
+    public IEnumerator mousewig()
+    {
+        bool play = true;
+
+        int count = 0;
+
+
+        while (play == true)
+        {
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+            float tmp = 0.0f;
+
+
+            if (x != 0.0f && y != 0.0f)
+            {
+                tmp = x * y;
+                tmp = tmp % 10.0f;
+                count++;
+                Number(tmp);
+
+            }
+            else if (x != 0.0f)
+            {
+                tmp = x;
+                tmp = tmp % 10.0f;
+                count++;
+                Number(tmp);
+
+            }
+            else if (y != 0.0f)
+            {
+                tmp = y;
+                tmp = tmp % 10.0f;
+                count++;
+                Number(tmp);
+            }
+
+
+            if (count == 3)
+            {
+                count = 0;
+                Instantiate(testObj, new Vector3(v1, v2, v3), Quaternion.identity);
+                v1 = 0.0f;
+                v2 = 0.0f;
+                v3 = 0.0f;
+            }
+
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+
+
 
     IEnumerator GenerateRandomVectors()
     {
