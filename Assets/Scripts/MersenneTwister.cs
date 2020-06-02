41fd6be47a486246f20b365d62dea83b8187dd3a
Vaughan Webb <vaughanw2012@gmail.com>
2020-06-02 18:24:25 +1200

    twister reformt with legal stuff

--- Assets/Scripts/MersenneTwister.cs
+++ Assets/Scripts/MersenneTwister.cs
@@ -53,7 +53,7 @@ public class MersenneTwister : MonoBehaviour
 
 		for (int k = N; k > 0; k--)
 		{
-			mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * (UInt32)1664525)) + (UInt32)seed[j] + (UInt32)j; /* non linear */
+			mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * (UInt32)1664525)) + (UInt32)seed[j] + (UInt32)j; 
 			i++;
 			j++;
 			if (i >= N)
@@ -69,7 +69,7 @@ public class MersenneTwister : MonoBehaviour
 
 		for (int k = N - 1; k > 0; k--)
 		{
-			mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * (UInt32)1566083941)) - (UInt32)i; /* non linear */
+			mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * (UInt32)1566083941)) - (UInt32)i; 
 			i++;
 			if (i >= N)
 			{
@@ -79,6 +79,59 @@ public class MersenneTwister : MonoBehaviour
 		}
 		mt[0] = 2147483648;
 	}
+
+	public double genrand_res53()
+	{
+		UInt32 a = genrand_Int32() >> 5;
+		UInt32 b = genrand_Int32() >> 6;
+		return ((double)a * 67108864.0 + b) * ((double)1.0 / 9007199254740992.0);
+	}
+
+/* 
+A C-program for MT19937, with initialization improved 2002/1/26.
+Coded by Takuji Nishimura and Makoto Matsumoto.
+
+Before using, initialize the state by using init_genrand(seed)  
+or init_by_array(init_key, key_length).
+
+Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+All rights reserved.                          
+Copyright (C) 2005, Mutsuo Saito,
+All rights reserved.                          
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+  1. Redistributions of source code must retain the above copyright
+	 notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright
+	 notice, this list of conditions and the following disclaimer in the
+	 documentation and/or other materials provided with the distribution.
+
+  3. The names of its contributors may not be used to endorse or promote 
+	 products derived from this software without specific prior written 
+	 permission.
+
+	 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	 LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	 A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+	 CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+	 EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	 PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	 PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	 LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	 NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+	 Any feedback is very welcome.
+	 http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+	 email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+	*/
+
 
 	public UInt32 genrand_Int32()
 	{
@@ -113,20 +166,5 @@ public class MersenneTwister : MonoBehaviour
 		y ^= (y >> 18);
 
 		return (y);
-	}
-
-	public double genrand_real1()       /* divided by 2^32-1 */
-	{
-		return genrand_Int32() * ((double)1.0 / 4294967295.0);
-	}
-	public double genrand_real2()       /* divided by 2^32 */
-	{
-		return genrand_Int32() * ((double)1.0 / 4294967296.0);
-	}
-	public double genrand_res53()       //with 53 - bit resolution
-	{
-		UInt32 a = genrand_Int32() >> 5;
-		UInt32 b = genrand_Int32() >> 6;
-		return ((double)a * 67108864.0 + b) * ((double)1.0 / 9007199254740992.0);
 	}
 }
